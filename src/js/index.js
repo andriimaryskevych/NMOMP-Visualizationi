a0d6d2fa74ec32bf12ddd6e9c301c312eb769309
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2018-04-30 19:42:23 +0300

    Added json files to awoid any parse concerns

--- src/js/index.js
+++ src/js/index.js
@@ -3,29 +3,7 @@ window.onload = () => {
     let height = window.innerHeight;
     let canvas = document.getElementById('canvas');
 
-    // var loader = new THREE.FileLoader();
-
-    // //load a text file and output the result to the console
-    // loader.load(
-    //     // resource URL
-    //     './points.txt',
-
-    //     // onLoad callback
-    //     function ( data ) {
-    //         // output the text to the console
-    //         window.data = data;
-    //     },
-
-    //     // onProgress callback
-    //     function ( xhr ) {
-    //         console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-    //     },
-
-    //     // onError callback
-    //     function ( err ) {
-    //         console.error( 'An error happened' );
-    //     }
-    // );
+    var loader = new THREE.FileLoader();    
     
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
@@ -36,25 +14,55 @@ window.onload = () => {
     let scene = new THREE.Scene();
 
     let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
-    camera.position.set(0, 0, 100);
+    camera.position.set(0, 0, 200);
     camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
 
     let light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
+
     
+    let startPoints;
+    let start;
 
-    var material = new THREE.LineBasicMaterial( { color: 0xffffff } );
+    loader.load('./start.txt', ( data ) => {
+            startPoints = JSON.parse(data);
 
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
-    geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
-    geometry.vertices.push(new THREE.Vector3( 10, 0, 10) );
+            var starsMaterial = new THREE.PointsMaterial( { color: 'red' } );
+            var geometry = new THREE.Geometry();
 
-    var line = new THREE.Line( geometry, material );
+            for(var i = 0; i < startPoints.length; i++)
+            {
+                geometry.vertices.push(new THREE.Vector3( startPoints[i].x * 20, startPoints[i].y * 20, startPoints[i].z * 20) );
+            }
 
-    scene.add(line);
+            start = new THREE.Points( geometry, starsMaterial );
+
+            scene.add(start);
+        }
+    );
+
+    let resultPoints;
+    let result;
+
+    loader.load('./points.txt', ( data ) => {
+            resultPoints = JSON.parse(data);
+
+            var starsMaterial = new THREE.PointsMaterial( { color: 'green' } );
+            var geometry = new THREE.Geometry();
+
+            for(var i = 0; i < resultPoints.length; i++)
+            {
+                geometry.vertices.push(new THREE.Vector3( resultPoints[i].x * 20, resultPoints[i].y * 20, resultPoints[i].z * 20) );
+            }
+
+            result = new THREE.Points( geometry, starsMaterial );
+
+            scene.add(result);
+        }
+    );
     
-    function loop() {
+    
+    function loop() {        
         
 
         renderer.render(scene, camera);
